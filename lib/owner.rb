d73a6362a8361c83b2246a27d13ab74d1ff299dc
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-05 16:43:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,14 @@
 class Owner
+  attr_accessor 
   @@all = []
+  def initialize(name)
+    @name=name
+  end 
   def self.all
     @@all
   end
+  def self.reset_all
+    @@all = []
+  end 
   # code goes here
 end
